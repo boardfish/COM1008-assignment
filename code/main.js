468aecb79315000ca9a73b12ae0f8a7fcebfcf4e
Simon Fish <simon@undyingfish.com>
2016-11-23 14:53:39 +0000

    Cleaning styles

--- code/main.js
+++ code/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var home = $('nav ul #home');
   var founders = $('nav ul #founders');
   var news = $('nav ul #news');
@@ -13,76 +13,74 @@ $(document).ready(function(){
   var contactContent = $('main #contactContent');
   var accessibilityContent = $('main #accessibilityContent');
   var puzzlesContent = $('main #puzzlesContent');
-
-  $(home).click(function () {
-	$(homeContent).slideDown();
-	$(foundersContent).hide();
-	$(newsContent).show();
-	$(recommendationsContent).hide();
-	$(contactContent).hide();
-	$(accessibilityContent).show();
-	$(puzzlesContent).hide();
-	return false;
+  $(home).click(function() {
+    $(homeContent).slideDown();
+    $(foundersContent).hide();
+    $(newsContent).show();
+    $(recommendationsContent).hide();
+    $(contactContent).hide();
+    $(accessibilityContent).show();
+    $(puzzlesContent).hide();
+    return false;
   });
-  $(founders).click(function () {
-	$(homeContent).hide();
-	$(foundersContent).slideDown();
-	$(newsContent).hide();
-	$(recommendationsContent).hide();
-	$(contactContent).hide();
-	$(accessibilityContent).hide();
-	$(puzzlesContent).hide();
-	return false;
+  $(founders).click(function() {
+    $(homeContent).hide();
+    $(foundersContent).slideDown();
+    $(newsContent).hide();
+    $(recommendationsContent).hide();
+    $(contactContent).hide();
+    $(accessibilityContent).hide();
+    $(puzzlesContent).hide();
+    return false;
   });
-  $(news).click(function () {
-	$(homeContent).hide();
-	$(foundersContent).hide();
-	$(newsContent).slideDown();
-	$(recommendationsContent).hide();
-	$(contactContent).hide();
-	$(accessibilityContent).hide();
-	$(puzzlesContent).hide();
-	return false;
+  $(news).click(function() {
+    $(homeContent).hide();
+    $(foundersContent).hide();
+    $(newsContent).slideDown();
+    $(recommendationsContent).hide();
+    $(contactContent).hide();
+    $(accessibilityContent).hide();
+    $(puzzlesContent).hide();
+    return false;
   });
-  $(recommendations).click(function () {
-	$(homeContent).hide();
-	$(foundersContent).hide();
-	$(newsContent).hide();
-	$(recommendationsContent).slideDown();
-	$(contactContent).hide();
-	$(accessibilityContent).hide();
-	$(puzzlesContent).hide();
-	return false;
+  $(recommendations).click(function() {
+    $(homeContent).hide();
+    $(foundersContent).hide();
+    $(newsContent).hide();
+    $(recommendationsContent).slideDown();
+    $(contactContent).hide();
+    $(accessibilityContent).hide();
+    $(puzzlesContent).hide();
+    return false;
   });
-  $(contact).click(function () {
-	$(homeContent).hide();
-	$(foundersContent).hide();
-	$(newsContent).hide();
-	$(recommendationsContent).hide();
-	$(contactContent).slideDown();
-	$(accessibilityContent).hide();
-	$(puzzlesContent).hide();
-	return false;
+  $(contact).click(function() {
+    $(homeContent).hide();
+    $(foundersContent).hide();
+    $(newsContent).hide();
+    $(recommendationsContent).hide();
+    $(contactContent).slideDown();
+    $(accessibilityContent).hide();
+    $(puzzlesContent).hide();
+    return false;
   });
-  $(accessibility).click(function () {
-	$(homeContent).hide();
-	$(foundersContent).hide();
-	$(newsContent).hide();
-	$(recommendationsContent).hide();
-	$(contactContent).hide();
-	$(accessibilityContent).slideDown();
-	$(puzzlesContent).hide();
-	return false;
+  $(accessibility).click(function() {
+    $(homeContent).hide();
+    $(foundersContent).hide();
+    $(newsContent).hide();
+    $(recommendationsContent).hide();
+    $(contactContent).hide();
+    $(accessibilityContent).slideDown();
+    $(puzzlesContent).hide();
+    return false;
   });
-  $(puzzles).click(function () {
-	$(homeContent).hide();
-	$(foundersContent).hide();
-	$(newsContent).hide();
-	$(recommendationsContent).hide();
-	$(contactContent).hide();
-	$(accessibilityContent).hide();
-	$(puzzlesContent).slideDown();
-	return false;
+  $(puzzles).click(function() {
+    $(homeContent).hide();
+    $(foundersContent).hide();
+    $(newsContent).hide();
+    $(recommendationsContent).hide();
+    $(contactContent).hide();
+    $(accessibilityContent).hide();
+    $(puzzlesContent).slideDown();
+    return false;
   });
-  
 });
