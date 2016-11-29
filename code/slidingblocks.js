4c056f51fa7175ec3012624b7282fe48bc24a2b3
Simon Fish <simon@undyingfish.com>
2016-11-29 16:45:01 +0000

    Initial shuffle implementation

--- code/slidingblocks.js
+++ code/slidingblocks.js
@@ -21,6 +21,7 @@ function swap(square) {
     grid[whiteX][whiteY] = square;
     console.log(square, "was swapped");
     redrawGrid()
+    winCheck()
   }
   else {
     console.log("That's an invalid move.");
@@ -29,14 +30,19 @@ function swap(square) {
 
 function redrawGrid() {
   console.log("Redrawing...");
+  console.log(grid);
   var firstStageSwapped = false;
   for (var i = 0; i < grid.length; i++) {
     for (var n = 0; n < grid[i].length; n++) {
+      console.log(grid[i][n]);
       var editedSquare = document.getElementById(grid[i][n]);
+      console.log(grid[i][n], "to", n*size/3, (grid.length-i-1)*size/3);
       editedSquare.setAttribute("x", n*size/3);
       editedSquare.setAttribute("y", (grid.length-i-1)*size/3);
     }
   }
+}
+function winCheck() {
   var winCondition = true;
   if (grid.length !== endGoalGrid.length) {
     winCondition = false;
@@ -70,10 +76,41 @@ function init() {
     swap(this.id)
   });
 }
+
+/*https://www.freecodecamp.com/challenges/generate-random-whole-numbers-with-javascript
+challenge gave me a lead in to how the random function works*/
+function nextLevel() {
+  console.log("NEXTLEVEL FUNCTION EXECUTED \N------------");
+  var colorList = ["red", "orange", "yellow", "green", "blue", "cyan","purple", "pink", "white"] //add more colours to this list to scale the program up to bigger grids.
+  var goalcolorList = ["red", "orange", "yellow", "green", "blue", "cyan", "purple", "pink", "white"];
+  var colorIndex;
+  var goalcolorIndex;
+  for (var i = 0; i < grid.length; i++) {
+    for (var n = 0; n < grid[0].length; n++) {
+      colorIndex = Math.floor(Math.random() * colorList.length);
+      goalcolorIndex = Math.floor(Math.random() * goalcolorList.length);
+      grid[i][n] = colorList[colorIndex];
+      endGoalGrid[i][n] = goalcolorList[goalcolorIndex];
+      console.log(i, n);
+      console.log("Index chosen was", colorIndex, "color", colorList[colorIndex]);
+      colorList.splice(colorIndex, 1);
+      console.log(colorList);
+      console.log(grid[i]);
+      console.log("Index chosen was", goalcolorIndex, "color", goalcolorList[goalcolorIndex]);
+      goalcolorList.splice(goalcolorIndex, 1);
+      console.log(goalcolorList);
+      console.log(endGoalGrid[i]);
+    }
+  }
+  redrawGrid()
+  $('#window').slideDown();
+  $('#victory').hide();
+}
+
 // global variables
-/*var grid = [["red", "green", "blue"], ["orange", "pink", "white"], ["cyan", "yellow", "purple"]];*/
+var grid = [["red", "green", "blue"], ["orange", "pink", "white"], ["cyan", "yellow", "purple"]];
 var size = $(window).width()/3;
-var grid = [["purple", "white", "pink"], ["green", "blue", "cyan"], ["red", "orange", "yellow"]];
+/*var grid = [["purple", "white", "pink"], ["green", "blue", "cyan"], ["red", "orange", "yellow"]];*/
 var endGoalGrid = [["purple", "pink", "white"], ["green", "blue", "cyan"], ["red", "orange", "yellow"]];
 // main program body
 redrawGrid();
